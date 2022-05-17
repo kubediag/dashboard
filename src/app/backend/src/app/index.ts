e37479d825f48b5b2a247e07acd7ede9cabbb385
jiangkun03 <jiangkun03@corp.netease.com>
2022-05-17 11:09:09 +0800

    Add k8s client-node api and related router

--- src/app/backend/src/app/index.ts
+++ src/app/backend/src/app/index.ts
@@ -1,10 +1,15 @@
 import Koa, { Context } from 'koa';
+import Router from './../router/api.router';
 import cors from 'koa2-cors';
 import bodyParser from 'koa-bodyparser';
 
 const app = new Koa();
 
-app.use(cors()).use(bodyParser());
+app
+  .use(cors())
+  .use(bodyParser())
+  .use(Router.routes())
+  .use(Router.allowedMethods());
 
 app.on('error', (err, ctx: Context) => {
   const status = 500;
